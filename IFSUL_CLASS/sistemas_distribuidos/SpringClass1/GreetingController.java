6b127c1965206b14455fda525f99f5aeea8427c7
Bruno Viera <brunovieramontes17@gmail.com>
2023-11-07 22:41:52 -0300

    commit lol

---
+++ IFSUL_CLASS/sistemas_distribuidos/SpringClass1/GreetingController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting") //cuando la requisicion sea map y el recurso sea greeting. Actua.
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
